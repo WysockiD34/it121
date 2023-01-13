0115cf95af721bd53e4e1d23ff1769861d457336
WysockiD34 <114697378+WysockiD34@users.noreply.github.com>
2023-01-13 02:15:31 +0000

    changed my index.html code

--- week 1/script.js
+++ week 1/script.js
@@ -2,9 +2,3 @@ let firstName = " Drew";
 let lastName = " Wysocki.";
 let yearsOfStudy = " one quarter";
 let goal = " apply to various jobs";
-let str1 = "My name is";
-let str2 = " I have been at SCC for";
-let str3 = " and plan to";
-let str4 = " after completing my courses.";
-
-alert(str1 + firstName + lastName + str2 + yearsOfStudy + str3 + goal + str4)
