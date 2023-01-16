97587ca72775421ecb008a36f50ad245bd92d3e5
WysockiD34 <114697378+WysockiD34@users.noreply.github.com>
2023-01-16 06:56:04 +0000

    made some changes and now it works as intended

--- PigLatin.js
+++ PigLatin.js
@@ -1,7 +1,8 @@
 let word = prompt("Please enter a word that has 2 or more letters in it")
 let length = word.length
-let check = word[0].indexOf("a","A","e","E","i","I","o","O","u","U")
-let check1 = word[1].indexOf("a","A","e","E","i","I","o","O","u","U")
+let vowels = "AaEeIiOoUu"
+let check = vowels.indexOf(word[0]);
+let check1 = vowels.indexOf(word[1]);
 if (length < 2) {
   prompt("Error, please enter a word longer than 1 character")
 };
@@ -9,7 +10,7 @@ if (length < 2) {
 if (check > -1) {
   console.log(word + "way")
 } else if (check1 === -1) {
-  console.log(word.substr(2) + "ay")
+  console.log(word.substr(2) + word.slice(0,2) + "ay")
 } else {
-  console.log(word.substr(1) + "ay")
+  console.log(word.substr(1) + word.slice(0,1) + "ay")
 }
